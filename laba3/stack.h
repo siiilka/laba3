40bb840e2d095bcd8ed093156c6d1551b99872b8
Usova Kseniya <kseniya_usova@icloud.com>
2025-12-14 18:50:13 +0300

    функции стека

--- laba3/stack.h
+++ laba3/stack.h
@@ -26,3 +26,33 @@ void stack_push(Stack* stack, StackElement element);
 StackElement stack_pop(Stack* stack);
 
 StackElement stack_top(const Stack* stack);
+
+
+
+
+
+StackElement stack_get(const Stack* stack, size_t index);
+
+StackElement* stack_begin(Stack* stack);
+StackElement* stack_end(Stack* stack);
+
+
+StackElement* stack_next(Stack* stack, StackElement* current);
+StackElement* stack_prev(Stack* stack, StackElement* current);
+
+
+void stack_insert_front(Stack* stack, StackElement element);
+void stack_insert_back(Stack* stack, StackElement element);
+void stack_insert_at(Stack* stack, size_t index, StackElement element);
+
+
+StackElement stack_remove_front(Stack* stack);
+StackElement stack_remove_back(Stack* stack);
+StackElement stack_remove_at(Stack* stack, size_t index);
+
+
+void stack_swap(Stack* stack, size_t index1, size_t index2);
+
+
+StackElement* stack_to_array(const Stack* stack);
+void stack_from_array(Stack* stack, StackElement* array, size_t size);
