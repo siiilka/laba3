daa14110c3182f4377630cd436cbb8225410694f
Usova Kseniya <kseniya_usova@icloud.com>
2025-12-22 22:41:22 +0300

    изменение названий переменных

--- comparators.h
+++ comparators.h
@@ -8,3 +8,5 @@ int publication_compare_by_year(const Publication* a, const Publication* b);
 int publication_compare_by_citations(const Publication* a, const Publication* b);
 int publication_compare_by_volume(const Publication* a, const Publication* b);
 int publication_compare_by_pages(const Publication* a, const Publication* b);
+
+
