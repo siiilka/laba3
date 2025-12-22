a4324c6fe2d3d01bd604e21f92875e5730ff0714
Usova Kseniya <kseniya_usova@icloud.com>
2025-12-22 22:16:07 +0300

    изменение названий переменных

--- internal.h
+++ internal.h
@@ -3,17 +3,18 @@
 #include "publication.h"
 #include <stdbool.h>
 #include <ctype.h>
+#include <stddef.h>
 
-void trim_string(char* str);
-bool is_all_digits(const char* str);
-bool parse_boolean(const char* str);
+void string_trim(char* str);
+bool string_is_all_digits(const char* str);
+bool string_parse_boolean(const char* str);
 
-bool validate_year(int year);
-bool validate_volume(int volume);
-bool validate_page_count(int page_count);
-bool validate_citations(int citations);
-bool validate_string_field(const char* field, size_t max_length);
+bool publication_validate_year(unsigned short year);
+bool publication_validate_volume(unsigned short volume);
+bool publication_validate_page_count(unsigned short page_count);
+bool publication_validate_citations(unsigned int citations);
+bool publication_validate_string_field(const char* field, size_t max_length);
 
-int count_csv_fields(const char* csv_line);
-char* get_csv_field(const char* csv_line, int field_index);
-void escape_csv_string(const char* src, char* dest, size_t dest_size);
+int csv_count_fields(const char* csv_line);
+char* csv_get_field(const char* csv_line, int field_index);
+void csv_escape_string(const char* src, char* dest, size_t dest_size);
